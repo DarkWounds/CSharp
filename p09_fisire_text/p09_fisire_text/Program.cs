02cd58f947910913ba059b3662545daac714d113
Paduraru Natalia-Maria <142657586+DarkWounds@users.noreply.github.com>
2025-09-22 13:26:59 +0300

    Fisier text

---
+++ p09_fisire_text/p09_fisire_text/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p09_fisire_text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Citim cuvinte din fisier si le afisam pe cate un rand
+            if (!File.Exists("Date.in"))
+            {
+                Console.WriteLine("Fisierul nu exista!");
+                return;
+            }
+
+            StreamReader fin = new StreamReader("Date.in");
+            StreamWriter fout = new StreamWriter("Date.out");
+
+            string linie;
+            while ((linie = fin.ReadLine()) != null)
+            {
+                string[] cuv = linie.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string e in cuv)
+                    fout.WriteLine("*" + e + "*");
+            }
+
+            fin.Close();
+            fout.Close();
+
+        }
+    }
+}
