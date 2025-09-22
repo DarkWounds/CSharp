c0e37a6359851a109ba888c67996ff39f719cfb5
Paduraru Natalia-Maria <142657586+DarkWounds@users.noreply.github.com>
2025-09-22 13:51:08 +0300

    Files 10,11

---
+++ p10_fisiere_text/p10_fisiere_text/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p11_fisiere_text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Vrem sa memoram in fisierul "fisiere.txt" lista
+             * fisierelor text din folderul "Date"
+             */
+            if (!Directory.Exists("Date"))
+            {
+                Console.WriteLine("Folder inexistent!");
+                return;
+            }
+
+            string[] f = Directory.GetFiles("Date", "*.txt");
+            StreamWriter fout = new StreamWriter("fisiere.txt");
+
+            foreach (string e in f)
+            {
+                int p = e.LastIndexOf("\\");
+                string fis = e.Substring(p + 1);
+                fout.WriteLine(fis);
+            }
+
+            fout.Close();
+        }
+    }
+}
---
+++ p11_Probleme/p11_Probleme/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace p11_Probleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p = Console.ReadLine();
+            int s;
+            s = int.Parse(p);
+
+            if( !File.Exists("patrate.in"))
+            {
+                Console.WriteLine("Nu exista fisierul");
+                return;
+            }
+
+            StreamWriter fout = new StreamWriter("patrate.in");
+
+            if (s < 1) 
+                fout.WriteLine("Nu exista");
+            else
+                for (int i = 1; i * i <= s; i++)
+                    fout.WriteLine(i);
+
+        }
+    }
+}
