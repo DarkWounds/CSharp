5d68b9e2b173b66ac982c4219bb115253994a1ed
Paduraru Natalia-Maria <142657586+DarkWounds@users.noreply.github.com>
2025-10-01 13:27:48 +0300

    Probleme de la test nr 1 , Rezolvate

--- p19_Test/p19_Test/Program.cs
+++ p19_Test/p19_Test/Program.cs
@@ -3,20 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
 
 namespace p19_Test
 {
     class Program
     {
+        /**
+          3 5 17 12
+          1 2 2 3
+          3 2
+         */
         static void Main(string[] args)
         {
-            string w;
-            int cnt = 0;
-            while(((w = Console.ReadLine()) != null))
+            //Problema 1 --------------------------------------
+            //In fisierul date.txt se afla [e mai multe randuri numere nat separate
+            //prin spatii. Sa se memoreze aceste numere intr-un tablou de nr intregi
+            //apoi se ordoneaza crescator
+            //Sa se afiseze la ecran pe prima linie numarul de numere din sir,
+            //pe a doua linie, numerele memorate in tablou,
+            //iar pe a treia linie numerele distincte care se gasesc in tablou.
+            /**
+             * Exemplu:
+                3 5 17 12
+                1 2 2 3
+                3 2
+             * Ecran:
+             * 10
+             * 1 2 2 2 3 3 5 12 17
+             * 1 2 3 5 12 17
+             */
+            if (!File.Exists("date.txt"))
             {
-                cnt++;
+                Console.WriteLine("Nu exista fisierul");
+                return;
             }
-            Console.WriteLine(cnt);
+
+            StreamReader fin = new StreamReader("date.txt");
+
+            ArrayList a = new ArrayList();
+         
+            string linie;
+
+            while ((linie = fin.ReadLine()) != null)
+            {
+                string[] ch = linie.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var e in ch)
+                    a.Add(int.Parse(e));
+            }
+
+            Console.WriteLine(a.Count);
+            a.Sort();
+
+            foreach (var e in a)
+                Console.Write(e + " ");
+            Console.WriteLine();
+
+            int[] fr = new int[a.Count];
+            foreach (int e in a)
+                fr[(int)e]++;
+
+            foreach (int e in a)
+                if (fr[(int)e] != 0)
+                {
+                    Console.Write(e + " ");
+                    fr[(int)e] = 0;
+                }
+            Console.WriteLine("\n");
         }
     }
 }
---
+++ p20_Test2/p20_Test2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace p20_Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problema 2-----------------------
+            //Se citeste de la tastaura un sir de caractere format numai din litere mici.
+            //Daca un caracter apare de mai multe ori, atunci sa se elimine toate aparitile sale,
+            //iar daca un carcater apare o singura data, atunci acesta va ramane in sir.
+            /*
+             *Exemplu:
+             *s = "abcdbcdbcddxyywb";
+             *Rasp:
+             *s = "axw";
+             */
+            string s = Console.ReadLine();
+            string aux = s;
+            string a = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                int lastIndex = 0, firstIndex = 0;
+                lastIndex = s.LastIndexOf(s[i]);
+                firstIndex = s.IndexOf(s[i]);
+                if (lastIndex == firstIndex)
+                    a += s[i];
+            }
+            s = a;
+            a = null;
+            Console.WriteLine(s);
+            
+        }
+    }
+}
