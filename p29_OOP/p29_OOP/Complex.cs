8c424200929bf32bb68a404599c2788026ff4946
Paduraru Natalia-Maria <142657586+DarkWounds@users.noreply.github.com>
2025-10-13 13:44:07 +0300

    OOP_NrComplexe

---
+++ p29_OOP/p29_OOP/Complex.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p29_OOP
+{
+    class Complex
+    {
+        private double re, im; // Var membre
+        public void Init(double x, double y) //Metoda
+        {
+            re = x;
+            im = y;
+        }
+        public void Init() //Metoda
+        {
+            re = im = 0;
+        }
+        public void Afis() //Metoda
+        {
+            Console.WriteLine(re + " + i * " + im);
+        }
+
+        public void Adunare(Complex z1, Complex z2)
+        {
+            this.re = z1.re + z2.re;
+            this.im = z1.im + z1.im;
+        }
+
+        public void Inmultire(Complex z1, Complex z2)
+        {
+            this.re = (z1.re * z2.re) - (z1.im * z2.im);
+            this.im = (z1.re * z2.im) + (z1.im * z2.re);
+        }
+
+        public void Impartire(Complex z1, Complex z2)
+        {
+            double numitor = z2.re * z2.re + z2.im * z2.im;
+            this.re = (z1.re * z2.re + z1.im * z2.im) / numitor;
+            this.im = (z1.im * z2.re - z1.re * z2.im) / numitor;
+        }
+
+        public double Modul()
+        {
+            return Math.Sqrt(re * re + im * im);
+        }
+
+        public override string ToString()
+        {
+            return re + " + i * " + im;
+        }
+
+        //--------------------Constructori-----------------------
+        public Complex(double x, double y)
+        {
+            Init(x, y);
+        }
+
+        public Complex()
+        {
+            re = im = 0;
+        }
+
+        public Complex(Complex z) // Constructor de copiere
+        {
+            re = z.re;
+            im = z.im;
+        }
+    }
+}
---
+++ p29_OOP/p29_OOP/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Clasa - un tip de data definit de utilizator 
+ * Impreuna cu operatiile de acest tip
+ * Instanta (obiect) - o variabila de tip clasa
+ * Variabila membra este o variabila definita intr-o clasa
+ * Metoda (functie membra) este o functie definita in clasa
+ * Datele membre sunt : 
+ * - Var memre
+ * - Metode
+ * - Constructori
+ * - Destructor
+ * - Proprietati
+ * - Indexatori
+ * - Operatori supradefiniti
+ * - Evenimente (Events)
+ * - Delegati (Delegates)
+ * Modificatori de acces: 
+ * - private (accesibile doar in clasa)
+ * - public (accesibile de orice)
+ * - protected (se comporta precum cele private, dar sunt accesibile si in clasele derivate)
+ * 
+ * OBS: Orice data membra a unei clase trebuie sa aiba specificat modificatorul de acces. Daca nu este mentionat, atunci 
+ * data membra respectiva este implicit private
+ * 
+ * Notiunile de baza/esentiale legate de OOP:
+ * - Incapsularea
+ * - Polimorfism
+ * - Mostenire
+ * 
+ * Constructorii:
+ * - Au rol de initializare
+ * - Poarta numele clasei
+ * - Nu returneaza nimic, nici macar void
+ * - Pot exista mai multi constructori care difera din parametrii
+ * (default = implicit, copy = de copiere, alti constructori)
+ * - Daca nu s-a definit in clasa un constructor se creeaza unul implicit
+ * - Pot fi public, protected, private
+ * - Constructorii se apeleaza implicit
+ * 
+ */
+
+namespace p29_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex z, z1, z2;
+            z = new Complex();
+            z1 = new Complex();
+            z2 = new Complex();
+            z1.Init(2, 5); z1.Afis();
+            z2.Init(3, 4); z2.Afis();
+
+            z.Adunare(z1, z2);
+            z.Afis();
+
+            z.Inmultire(z1, z2);
+            z.Afis();
+
+            z.Impartire(z1, z2);
+            z.Afis();
+
+            Console.WriteLine(z2.Modul());
+
+            Console.WriteLine(z1);
+
+            Complex z3 = new Complex(z1);
+            z3.Afis();
+        }
+    }
+}
