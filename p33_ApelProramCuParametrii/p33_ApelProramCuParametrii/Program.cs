44e0d3d16243550c8a134bc4db7692d5beb453b1
Paduraru Natalia-Maria <142657586+DarkWounds@users.noreply.github.com>
2025-11-12 12:38:46 +0200

    Functii statice, Deschidere app cu args, afis cons

---
+++ p33_ApelProramCuParametrii/p33_ApelProramCuParametrii/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p33_ApelProramCuParametrii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Argumentele introduse sunt in nr de " + args.Length);
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Lista argumentelor:");
+                foreach (var e in args)
+                    Console.WriteLine(e);
+            }
+            else Console.WriteLine("N-am argumente");
+
+            Console.ReadKey();
+        }
+    }
+}
