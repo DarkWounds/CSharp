7232672c2489f3544e520c10cd2470416cba01f7
Paduraru Natalia-Maria <142657586+DarkWounds@users.noreply.github.com>
2025-11-17 12:54:00 +0200

    Add files via upload

---
+++ p35_Template/p35_Template/Stiva.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p35_Template
+{
+    /// <summary>
+    /// Lista inlantuita de tip template
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// 
+
+    class Nod<T>
+    {
+        public T info;
+        public Nod<T> leg;
+        public Nod(T val)
+        {
+            info = val;
+            leg = null;
+        }
+    }
+
+    class Stiva<T>
+    {
+        public Nod<T> top;
+        int n; // numarul de noduri
+
+        public void Init()
+        {
+            top = null;
+            n = 0;
+        }
+
+        public Stiva()
+        {
+            Init();
+        }
+
+        public int Size
+        {
+            get { return n; }
+        }
+
+        //Adauga pe x in stiva
+        public void Push(T x)
+        {
+            Nod<T> p = new Nod<T>(x);
+            p.leg = top;
+            top = p;
+            n++;
+        }
+
+        public void Pop()
+        {
+            Nod<T> p = top;
+            top = top.leg;
+            p = null;
+            n--;
+        }
+
+        public T Top()
+        {
+            return top.info;
+        }
+
+        public bool Empty()
+        {
+            return (top == null);
+        }
+
+    }
+}
