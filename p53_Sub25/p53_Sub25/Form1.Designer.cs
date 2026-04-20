ff62f0851701d66c74f42b1aecc80180555f3477
Paduraru Natalia-Maria <142657586+DarkWounds@users.noreply.github.com>
2026-04-20 13:47:39 +0300

    Subiecte

---
+++ p53_Sub25/p53_Sub25/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace p53_Sub25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxCod = new System.Windows.Forms.TextBox();
+            this.textBoxAdresa = new System.Windows.Forms.TextBox();
+            this.textBoxEtaj = new System.Windows.Forms.TextBox();
+            this.textBoxCamere = new System.Windows.Forms.TextBox();
+            this.textBoxPret = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonIesire = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBoxAfis = new System.Windows.Forms.ListBox();
+            this.buttonAfisNrAp = new System.Windows.Forms.Button();
+            this.buttonAfisPretMinim = new System.Windows.Forms.Button();
+            this.buttonAfisCamereEgal = new System.Windows.Forms.Button();
+            this.textBoxEgalCamere = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adauga apartament:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cod";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(66, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Adresa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(66, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Etaj";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(67, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nr camere";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(67, 291);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Pret";
+            // 
+            // textBoxCod
+            // 
+            this.textBoxCod.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCod.Location = new System.Drawing.Point(211, 134);
+            this.textBoxCod.Name = "textBoxCod";
+            this.textBoxCod.Size = new System.Drawing.Size(73, 29);
+            this.textBoxCod.TabIndex = 6;
+            // 
+            // textBoxAdresa
+            // 
+            this.textBoxAdresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAdresa.Location = new System.Drawing.Point(211, 175);
+            this.textBoxAdresa.Name = "textBoxAdresa";
+            this.textBoxAdresa.Size = new System.Drawing.Size(197, 29);
+            this.textBoxAdresa.TabIndex = 7;
+            // 
+            // textBoxEtaj
+            // 
+            this.textBoxEtaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEtaj.Location = new System.Drawing.Point(211, 217);
+            this.textBoxEtaj.Name = "textBoxEtaj";
+            this.textBoxEtaj.Size = new System.Drawing.Size(73, 29);
+            this.textBoxEtaj.TabIndex = 8;
+            // 
+            // textBoxCamere
+            // 
+            this.textBoxCamere.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCamere.Location = new System.Drawing.Point(211, 257);
+            this.textBoxCamere.Name = "textBoxCamere";
+            this.textBoxCamere.Size = new System.Drawing.Size(73, 29);
+            this.textBoxCamere.TabIndex = 9;
+            // 
+            // textBoxPret
+            // 
+            this.textBoxPret.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPret.Location = new System.Drawing.Point(211, 292);
+            this.textBoxPret.Name = "textBoxPret";
+            this.textBoxPret.Size = new System.Drawing.Size(73, 29);
+            this.textBoxPret.TabIndex = 10;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(72, 344);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(249, 65);
+            this.buttonAdd.TabIndex = 11;
+            this.buttonAdd.Text = "Adauga apartament";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonIesire
+            // 
+            this.buttonIesire.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIesire.Location = new System.Drawing.Point(71, 470);
+            this.buttonIesire.Name = "buttonIesire";
+            this.buttonIesire.Size = new System.Drawing.Size(165, 65);
+            this.buttonIesire.TabIndex = 12;
+            this.buttonIesire.Text = "Exit";
+            this.buttonIesire.UseVisualStyleBackColor = true;
+            this.buttonIesire.Click += new System.EventHandler(this.buttonIesire_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(510, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(224, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Adauga apartament:";
+            // 
+            // listBoxAfis
+            // 
+            this.listBoxAfis.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxAfis.FormattingEnabled = true;
+            this.listBoxAfis.ItemHeight = 24;
+            this.listBoxAfis.Location = new System.Drawing.Point(515, 77);
+            this.listBoxAfis.Name = "listBoxAfis";
+            this.listBoxAfis.Size = new System.Drawing.Size(324, 244);
+            this.listBoxAfis.TabIndex = 14;
+            // 
+            // buttonAfisNrAp
+            // 
+            this.buttonAfisNrAp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAfisNrAp.Location = new System.Drawing.Point(515, 344);
+            this.buttonAfisNrAp.Name = "buttonAfisNrAp";
+            this.buttonAfisNrAp.Size = new System.Drawing.Size(324, 56);
+            this.buttonAfisNrAp.TabIndex = 15;
+            this.buttonAfisNrAp.Text = "Afis numar apartamente";
+            this.buttonAfisNrAp.UseVisualStyleBackColor = true;
+            this.buttonAfisNrAp.Click += new System.EventHandler(this.buttonAfisNrAp_Click);
+            // 
+            // buttonAfisPretMinim
+            // 
+            this.buttonAfisPretMinim.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAfisPretMinim.Location = new System.Drawing.Point(515, 406);
+            this.buttonAfisPretMinim.Name = "buttonAfisPretMinim";
+            this.buttonAfisPretMinim.Size = new System.Drawing.Size(324, 53);
+            this.buttonAfisPretMinim.TabIndex = 18;
+            this.buttonAfisPretMinim.Text = "Afis ap nr minim";
+            this.buttonAfisPretMinim.UseVisualStyleBackColor = true;
+            this.buttonAfisPretMinim.Click += new System.EventHandler(this.buttonAfisPretMinim_Click);
+            // 
+            // buttonAfisCamereEgal
+            // 
+            this.buttonAfisCamereEgal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAfisCamereEgal.Location = new System.Drawing.Point(681, 470);
+            this.buttonAfisCamereEgal.Name = "buttonAfisCamereEgal";
+            this.buttonAfisCamereEgal.Size = new System.Drawing.Size(158, 55);
+            this.buttonAfisCamereEgal.TabIndex = 19;
+            this.buttonAfisCamereEgal.Text = "Afis ap cu nr camere";
+            this.buttonAfisCamereEgal.UseVisualStyleBackColor = true;
+            this.buttonAfisCamereEgal.Click += new System.EventHandler(this.buttonAfisCamereEgal_Click);
+            // 
+            // textBoxEgalCamere
+            // 
+            this.textBoxEgalCamere.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEgalCamere.Location = new System.Drawing.Point(608, 486);
+            this.textBoxEgalCamere.Name = "textBoxEgalCamere";
+            this.textBoxEgalCamere.Size = new System.Drawing.Size(67, 29);
+            this.textBoxEgalCamere.TabIndex = 21;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(511, 494);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 20);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Nr camere";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 591);
+            this.Controls.Add(this.textBoxEgalCamere);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.buttonAfisCamereEgal);
+            this.Controls.Add(this.buttonAfisPretMinim);
+            this.Controls.Add(this.buttonAfisNrAp);
+            this.Controls.Add(this.listBoxAfis);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.buttonIesire);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxPret);
+            this.Controls.Add(this.textBoxCamere);
+            this.Controls.Add(this.textBoxEtaj);
+            this.Controls.Add(this.textBoxAdresa);
+            this.Controls.Add(this.textBoxCod);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxCod;
+        private System.Windows.Forms.TextBox textBoxAdresa;
+        private System.Windows.Forms.TextBox textBoxEtaj;
+        private System.Windows.Forms.TextBox textBoxCamere;
+        private System.Windows.Forms.TextBox textBoxPret;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonIesire;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBoxAfis;
+        private System.Windows.Forms.Button buttonAfisNrAp;
+        private System.Windows.Forms.Button buttonAfisPretMinim;
+        private System.Windows.Forms.Button buttonAfisCamereEgal;
+        private System.Windows.Forms.TextBox textBoxEgalCamere;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ p53_Sub25/p53_Sub25/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p53_Sub25
+{
+    public partial class Form1 : Form
+    {
+        string connString;
+        public Form1()
+        {
+            InitializeComponent();
+            connString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\12A\p53_Sub25\p53_Sub25\apartamente.mdf;Integrated Security=True";
+        }
+
+        private void buttonIesire_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string adresa;
+            int cod, pret, camere, etajul;
+
+
+            cod = int.Parse(textBoxCod.Text.Trim());
+            adresa = textBoxAdresa.Text.Trim();
+            pret = int.Parse(textBoxPret.Text.Trim());
+            camere = int.Parse(textBoxCamere.Text.Trim());
+            etajul = int.Parse(textBoxEtaj.Text.Trim());
+
+            if (cod == null || adresa == String.Empty || pret == null || camere == null || etajul == null)
+            {
+                MessageBox.Show("Date incomplete");
+                return;
+            }
+
+            int nr = 0;
+            //Caut cod
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"select count(*) from apartament where cod = '" + cod + @"'";
+                using (SqlDataReader r = cmd.ExecuteReader())
+                {
+                    r.Read();
+                    nr = (int)r[0];
+                    r.Close();
+                }
+                conn.Close();
+            }
+
+            if (nr != 0)
+            {
+                MessageBox.Show("Exista deja produsul");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"insert into apartament (cod, adresa, etajul ,camere, pret) values (" + cod + @", '" + adresa + @"' , " + etajul + @" ," + camere + @", " + pret + @")";
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                textBoxCamere.Text = "";
+                textBoxCod.Text = "";
+                textBoxEtaj.Text = "";
+                textBoxPret.Text = "";
+                textBoxAdresa.Text = "";
+                textBoxCod.Focus();
+
+            }
+        }
+
+        private void buttonAfisNrAp_Click(object sender, EventArgs e)
+        {
+            int nr = 0;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"select count(*) as nr from apartament";
+                using (SqlDataReader r = cmd.ExecuteReader())
+                {
+                    r.Read();
+                    nr = (int)r[0];
+                    r.Close();
+                }
+                conn.Close();
+            }
+            listBoxAfis.Items.Clear();
+            listBoxAfis.Items.Add(nr);
+        }
+
+        private void buttonAfisPretMinim_Click(object sender, EventArgs e)
+        {
+            int nr = 0;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"select * from apartament";
+                listBoxAfis.Items.Clear();
+                using (SqlDataReader r = cmd.ExecuteReader())
+                {
+                    while (r.Read())
+                    {
+                        int s = ';
+                        if()
+                        string w = ;
+                        listBoxAfis.Items.Add(w);
+                    }
+                    r.Close();
+                }
+                conn.Close();
+            }
+        }
+
+        private void buttonAfisCamereEgal_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                int valoare = Int32.MaxValue;
+                string v = textBoxEgalCamere.Text.Trim();
+                if (v != String.Empty)
+                    valoare = int.Parse(v);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"select cod,adresa,etajul from apartament where camere = " + valoare.ToString();
+                listBoxAfis.Items.Clear();
+                using (SqlDataReader r = cmd.ExecuteReader())
+                {
+                    while (r.Read())
+                    {
+                        string w = r[0].ToString() + " - " + r[1].ToString() + " - " + r[2].ToString();
+                        listBoxAfis.Items.Add(w);
+                    }
+                    r.Close();
+                }
+                conn.Close();
+            }
+        }
+    }
+}
