fc34b4e29da930b8db66d03e169b5baa5d0e5cb7
Paduraru Natalia-Maria <142657586+DarkWounds@users.noreply.github.com>
2026-04-22 13:44:48 +0300

    Subiect atestat nr 21

---
+++ p55_SubiectAtestat21/p55_SubiectAtestat21/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace p55_SubiectAtestat21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxCod = new System.Windows.Forms.TextBox();
+            this.textBoxNume = new System.Windows.Forms.TextBox();
+            this.textBoxPartid = new System.Windows.Forms.TextBox();
+            this.textBoxVechime = new System.Windows.Forms.TextBox();
+            this.buttonAddMembrii = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonIesire = new System.Windows.Forms.Button();
+            this.buttonAfisNrMembriiPartide = new System.Windows.Forms.Button();
+            this.buttonAfisMembriiPartidSpecific = new System.Windows.Forms.Button();
+            this.textBoxAfispartid = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(69, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add parlamentari noi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(69, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Vechime:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Partid:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(69, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Cod:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(69, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nume:";
+            // 
+            // textBoxCod
+            // 
+            this.textBoxCod.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCod.Location = new System.Drawing.Point(214, 123);
+            this.textBoxCod.Name = "textBoxCod";
+            this.textBoxCod.Size = new System.Drawing.Size(117, 31);
+            this.textBoxCod.TabIndex = 5;
+            // 
+            // textBoxNume
+            // 
+            this.textBoxNume.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNume.Location = new System.Drawing.Point(191, 181);
+            this.textBoxNume.Name = "textBoxNume";
+            this.textBoxNume.Size = new System.Drawing.Size(140, 31);
+            this.textBoxNume.TabIndex = 6;
+            // 
+            // textBoxPartid
+            // 
+            this.textBoxPartid.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPartid.Location = new System.Drawing.Point(156, 243);
+            this.textBoxPartid.Name = "textBoxPartid";
+            this.textBoxPartid.Size = new System.Drawing.Size(175, 31);
+            this.textBoxPartid.TabIndex = 7;
+            // 
+            // textBoxVechime
+            // 
+            this.textBoxVechime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxVechime.Location = new System.Drawing.Point(214, 298);
+            this.textBoxVechime.Name = "textBoxVechime";
+            this.textBoxVechime.Size = new System.Drawing.Size(116, 31);
+            this.textBoxVechime.TabIndex = 8;
+            // 
+            // buttonAddMembrii
+            // 
+            this.buttonAddMembrii.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddMembrii.Location = new System.Drawing.Point(75, 357);
+            this.buttonAddMembrii.Name = "buttonAddMembrii";
+            this.buttonAddMembrii.Size = new System.Drawing.Size(256, 58);
+            this.buttonAddMembrii.TabIndex = 9;
+            this.buttonAddMembrii.Text = "Adauga parlamentar";
+            this.buttonAddMembrii.UseVisualStyleBackColor = true;
+            this.buttonAddMembrii.Click += new System.EventHandler(this.buttonAddMembrii_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(389, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(296, 304);
+            this.listBox1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(384, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Afisari:";
+            // 
+            // buttonIesire
+            // 
+            this.buttonIesire.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonIesire.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIesire.Location = new System.Drawing.Point(937, 458);
+            this.buttonIesire.Name = "buttonIesire";
+            this.buttonIesire.Size = new System.Drawing.Size(133, 58);
+            this.buttonIesire.TabIndex = 12;
+            this.buttonIesire.Text = "Iesire";
+            this.buttonIesire.UseVisualStyleBackColor = true;
+            this.buttonIesire.Click += new System.EventHandler(this.buttonIesire_Click);
+            // 
+            // buttonAfisNrMembriiPartide
+            // 
+            this.buttonAfisNrMembriiPartide.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAfisNrMembriiPartide.Location = new System.Drawing.Point(691, 99);
+            this.buttonAfisNrMembriiPartide.Name = "buttonAfisNrMembriiPartide";
+            this.buttonAfisNrMembriiPartide.Size = new System.Drawing.Size(256, 58);
+            this.buttonAfisNrMembriiPartide.TabIndex = 13;
+            this.buttonAfisNrMembriiPartide.Text = "Afis numar membrii partide";
+            this.buttonAfisNrMembriiPartide.UseVisualStyleBackColor = true;
+            this.buttonAfisNrMembriiPartide.Click += new System.EventHandler(this.buttonAfisNrMembriiPartide_Click);
+            // 
+            // buttonAfisMembriiPartidSpecific
+            // 
+            this.buttonAfisMembriiPartidSpecific.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAfisMembriiPartidSpecific.Location = new System.Drawing.Point(691, 229);
+            this.buttonAfisMembriiPartidSpecific.Name = "buttonAfisMembriiPartidSpecific";
+            this.buttonAfisMembriiPartidSpecific.Size = new System.Drawing.Size(256, 58);
+            this.buttonAfisMembriiPartidSpecific.TabIndex = 14;
+            this.buttonAfisMembriiPartidSpecific.Text = "Afisare membrii de la partidul:";
+            this.buttonAfisMembriiPartidSpecific.UseVisualStyleBackColor = true;
+            this.buttonAfisMembriiPartidSpecific.Click += new System.EventHandler(this.buttonAfisMembriiPartidSpecific_Click);
+            // 
+            // textBoxAfispartid
+            // 
+            this.textBoxAfispartid.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAfispartid.Location = new System.Drawing.Point(759, 304);
+            this.textBoxAfispartid.Name = "textBoxAfispartid";
+            this.textBoxAfispartid.Size = new System.Drawing.Size(117, 31);
+            this.textBoxAfispartid.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(572, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonIesire;
+            this.ClientSize = new System.Drawing.Size(1096, 545);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxAfispartid);
+            this.Controls.Add(this.buttonAfisMembriiPartidSpecific);
+            this.Controls.Add(this.buttonAfisNrMembriiPartide);
+            this.Controls.Add(this.buttonIesire);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonAddMembrii);
+            this.Controls.Add(this.textBoxVechime);
+            this.Controls.Add(this.textBoxPartid);
+            this.Controls.Add(this.textBoxNume);
+            this.Controls.Add(this.textBoxCod);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxCod;
+        private System.Windows.Forms.TextBox textBoxNume;
+        private System.Windows.Forms.TextBox textBoxPartid;
+        private System.Windows.Forms.TextBox textBoxVechime;
+        private System.Windows.Forms.Button buttonAddMembrii;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonIesire;
+        private System.Windows.Forms.Button buttonAfisNrMembriiPartide;
+        private System.Windows.Forms.Button buttonAfisMembriiPartidSpecific;
+        private System.Windows.Forms.TextBox textBoxAfispartid;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ p55_SubiectAtestat21/p55_SubiectAtestat21/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p55_SubiectAtestat21
+{
+    public partial class Form1 : Form
+    {
+        string connString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=" + Pozitie() + ";Integrated Security=True";
+        }
+
+        private string Pozitie()
+        {
+            string dir = Directory.GetCurrentDirectory();
+            int p = dir.LastIndexOf(@"\bin\");
+            string sub = dir.Substring(0, p + 1) + @"parlament.mdf";
+            return sub;
+        }
+
+        private void buttonAddMembrii_Click(object sender, EventArgs e)
+        {
+            string c = textBoxCod.Text.ToString().Trim();
+            string nume = textBoxNume.Text.ToString().Trim();
+            string partid = textBoxPartid.Text.ToString().Trim();
+            string vechi = textBoxVechime.Text.ToString().Trim();
+
+            if (nume == null || partid == null || partid == null || vechi == null)
+            {
+                MessageBox.Show("Date incomplete");
+                ClearAll();
+            }
+
+            int cod = int.Parse(c.Trim());
+            int vechime = int.Parse(vechi.Trim());
+            int nr = 0;
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"select count(*) from membrii where cod = '" + cod + @"'";
+                using (SqlDataReader r = cmd.ExecuteReader())
+                {
+                    r.Read();
+                    nr = (int)r[0];
+                    r.Close();
+                }
+                conn.Close();
+            }
+
+            if (nr != 0)
+            {
+                MessageBox.Show("Exista deja produsul");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"insert into membrii (cod, numele, partid, vechime) values (" + cod + @", '" + nume + @"' , '" + partid + @"', " + vechime + @")";
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                ClearAll();
+
+            }
+
+            listBox1.Items.Clear();
+               
+        }
+
+        private void ClearAll()
+        {
+            textBoxCod.Text = "";
+            textBoxNume.Text = "";
+            textBoxPartid.Text = "";
+            textBoxVechime.Text = "";
+            textBoxAfispartid.Text = "";
+        }
+
+        private void buttonIesire_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonAfisNrMembriiPartide_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+            listBox1.Items.Clear();
+
+            List<string> lista = new List<string>();
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"select partid from membrii";
+
+                using (SqlDataReader r = cmd.ExecuteReader())
+                {
+                    while (r.Read())
+                    {
+                        string t = r[0].ToString();
+                        lista.Add(t);
+                    }
+                    r.Close();
+                }
+
+                conn.Close();
+            } 
+
+            for (int i = 0; i < lista.Count(); i++)
+            {
+                int nr = 0;
+                string partid = lista[i].ToString();
+
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"select count(*) as nr from membrii where partid = '" + partid + "'";
+
+                    using (SqlDataReader r = cmd.ExecuteReader())
+                    {
+                        r.Read();
+                        nr = (int)r[0];
+                        r.Close();
+                    }
+                    conn.Close();
+                }
+
+                listBox1.Items.Add(lista[i] + ": " + nr);
+            }
+
+            ClearAll();
+        }
+
+        private void buttonAfisMembriiPartidSpecific_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+
+            string partid = textBoxAfispartid.Text.ToString().Trim();
+
+            if (partid == String.Empty)
+            {
+                MessageBox.Show("Nu ai scris un partid");
+                ClearAll();
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = @"select numele as nr from membrii where partid = '" + partid + "'";
+
+                listBox1.Items.Add(partid + ": ");
+
+                using (SqlDataReader r = cmd.ExecuteReader())
+                {
+                    while (r.Read())
+                    {
+                        string t = r[0].ToString();
+                        listBox1.Items.Add(t);
+                    }
+                    r.Close();
+                }
+                conn.Close();
+            }
+        }
+    }
+}
